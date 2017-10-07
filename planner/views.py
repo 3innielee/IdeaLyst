0c4ae913ce2f4de0f5781fa2dbea7bbf20c4ddac
robgle <robby.gleiser@gmail.com>
2017-10-07 06:09:44 -0700

    Add date scroll feature

--- planner/views.py
+++ planner/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . import models
 from django.views.generic import TemplateView
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 class DayView(TemplateView):
@@ -12,7 +12,11 @@ class DayView(TemplateView):
         if self.kwargs.get('pk'):
             pk = self.kwargs.get('pk')
             date = datetime(year=int(pk[0:4]), month=int(pk[4:6]), day=int(pk[6:8]))
+            yesterday = date - timedelta(days=1)
+            tmrw = date + timedelta(days=1)
             context['valid'] = True
+            context['yesterday'] = yesterday.strftime("%Y%m%d")
+            context['tmrw'] = tmrw.strftime("%Y%m%d")
             context['day'] = date.strftime('%m-%d-%Y')
             context['events'] = models.Event.objects.filter(author=self.request.user, day=date)
         else:
